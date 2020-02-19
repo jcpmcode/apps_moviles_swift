c882389a0fdfc757fde6d276e92a1726f1ec8fdb
jcpmcode <jcpmcode@gmail.com>
2020-02-18 19:03:24 -0600

    X

--- practicas/practica1/segunda_parte/segunda_parte/main.swift
+++ practicas/practica1/segunda_parte/segunda_parte/main.swift
@@ -8,5 +8,66 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Descripción
+print("Hola, este es un programa que genera el RFC tanto para personas fisicas como para personas morales,. A continuacion encontraras un sencillo menú con 3 opciones:")
 
+
+// MARK: - Menu principal
+repeat
+print("\t1. Generar RFC para persona fisica")
+print("\t2. Generar RFC para persona moral")
+print("\t3. Salir de la aplicación")
+while (1)
+
+
+print ("Quieres generar otro RFC?")
+print("\t1. Si (Regresar al menu anterior)")
+print("\t2. No. (Salir del programa)")
+
+
+// MARK: - Personas fisicas
+//  3 letras, 1 espacio y 6 digitos. 3 digitos adicionales para la homoclave (2 letras y 1 numero)
+print("Ingresa los datos de la persona fisica a registrar: ")
+print("Apellido paterno: ")
+print("Apellido Materno: ")
+print("Nombre(s)")
+
+
+// MARK: - Personas morales
+// 4 letras y 6 digitos 3 digitos adicionales para la homoclave (2 letras y 1 numero)
+print("Ingresa la razon social de la empresa: ")
+print("Ingrese la fecha de creacion de la empresa: ")
+print("Año: ")
+print("Mes: ")
+print("Día: ")
+
+
+
+// 1
+if (contadorDePalabras >= 0) {
+    if (contadorDePalabras >= 3) {
+        
+    } else {
+        // regla 6
+        if (contadorDePalabras == 2) {
+            
+        }
+    }
+}
+else {
+    
+}
+
+
+
+Algoritmo para tomar digitos de  la fecha:
+
+Algoritmo para
+
+Diccionarios Personas fisicas:
+Tabla VI
+
+Diccionarios personas morales:
+Tabla V
+
+    
